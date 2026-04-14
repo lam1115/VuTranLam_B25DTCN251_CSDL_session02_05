e3d1873fbae09274dd250eb8d9e830678308136e
lam1115 <vutranlam1115@gmail.com>
2026-04-14 22:54:56 +0700

    Add files via upload

---
+++ Bai_5.sql
@@ -0,0 +1,22 @@
+-- 3 kịch bản mà người dùng có thể vô tình hoặc cố ý tạo ra:
+-- Nhập số tiền không hợp lệ: Người dùng nhập số tiền âm nhằm hack hệ thống để tăng số dư, Nhập 0 đồng để tạo record rác, Nhập số tiền quá nhỏ hoặc quá lớn.
+-- Thực hiện giao dịch khi không đủ số dư.
+-- Định dang sai đơn vị tiền tệ. (VND nhưng nhập số tiền dưới dạng số thực (float)).
+
+CREATE DATABASE e_wallet;
+USE e_wallet;
+
+CREATE TABLE users (
+	userId INT PRIMARY KEY,
+    email VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE e_wallet (
+	walletId INT PRIMARY KEY,
+    balance DECIMAL(10,0) NOT NULL DEFAULT 0 CHECK(balance >= 0),
+    userId INT NOT NULL UNIQUE,
+    FOREIGN KEY (userId) references users(userId)
+);
+
+SELECT * FROM users;
+SELECT * FROM e_wallet;
